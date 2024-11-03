ee391e5eae26fd9793f2a1baca046e270cb12bfd
Noor Mohammed Anik <noormohammedanik@gmail.com>
2024-11-04 00:57:10 +0600

    - updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_receipe_app/splash_screen.dart';
 
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      backgroundColor: Colors.white,  // Professional, clean background
+      enabled: true,                 // Let the magic begin!
+      defaultDevice: Devices.ios.iPhone13ProMax,  // Start with a bang
+      isToolbarVisible: true,                     // Give users control
+      availableLocales: [Locale('en', 'US')],    // Keep it focused
+        tools: const [
+          DeviceSection(
+            model: true,          // Let them switch devices
+            orientation: false,   // Keep it simple
+            frameVisibility: false, // Clean presentation
+            virtualKeyboard: false, // Focus on the UI
+          ),
+        ],
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Recipe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
